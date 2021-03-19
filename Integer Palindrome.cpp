d2aa8478db45fad4f90a83c2654c7d7fb308c073
dickyryana <80949620+dickyryana@users.noreply.github.com>
2021-03-19 16:11:55 +0700

    Membuat File Integer Palindrome

---
+++ Integer Palindrome.cpp
@@ -0,0 +1,29 @@
+#include <stdio.h>
+int main()
+{
+    int n, a, reversedInteger = 0, remainder, jumlah=0, originalInteger;
+    
+    printf("masukan jumlah data : ");
+    scanf("%d",&n);
+    for(int i=1; i<=n; i++)
+    {
+            reversedInteger = 0;
+            printf("Masukan angka : ");
+            scanf("%d",&a);
+            originalInteger = a;
+            
+            while (a!=0)
+            {
+                remainder = a%10;
+                reversedInteger = reversedInteger*10 + remainder;
+                a /= 10;
+                if (originalInteger == reversedInteger)
+                {
+                    jumlah = jumlah + 1;
+                }
+            }
+                    
+    }
+                    printf("\nBanyak angka polindrome : %d \n", jumlah);
+    
+}
